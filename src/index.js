4c9e444dfcc60167e0e85dbdbc3f86fc9bf07028
Saurabh kumar <1213saurabhsingh1213@gmail.com>
2024-12-22 21:56:57 +0530

    add user and video model and utils(ApiError,ApiResponse,asyncHandler)

--- src/index.js
+++ src/index.js
@@ -4,4 +4,10 @@ dotenv.config({
     path:'./env'
 })
 
-connectDB();
+connectDB().then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is running at port :${process.env.PORT}`);
+    })
+}).catch((error)=>{
+    console.log("MONGO DB CONNECTION FAILED !!! ",error);
+})
